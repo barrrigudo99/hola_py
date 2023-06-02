5bff4ad54d373087843e581284b96560ece2126b
barrrigudo99 <122281078+barrrigudo99@users.noreply.github.com>
2023-06-02 14:06:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/")
 def hola():
-    return "<h1> Bienvenido al servidor web en python </h1>"
+    return render_template('index.html')
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run()
-# Para lanzar el servidor flask -app nombre run
